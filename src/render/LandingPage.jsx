7efd8521d376518983376764f59e06f4ee9a2abe
gioiamicaela <gioiamicaela@gmail.com>
2023-03-01 20:54:21 +0100

    sidebar

--- src/render/LandingPage.jsx
+++ src/render/LandingPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <div
+        style={{
+          float: "left",
+          width: "17%",
+          height: "100vh",
+          color: "#FFFFFF",
+          border: "1px solid #F3F3F3",
+        }}
+      >
+        SideBar
+      </div>
+      <div
+        style={{
+          float: "Right",
+          backgroundColor: "#F3F3F3",
+          width: "83%",
+          height: "100vh",
+        }}
+      >
+        SideBar
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
